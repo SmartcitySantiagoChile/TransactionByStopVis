14c168cf987b76e0aa3e042dfc35ffe0017cd66d
bastianleaf <bastian.martinez@ing.uchile.cl>
2020-05-26 13:25:00 -0400

    Se agrega constructor de output en script.

--- process_data.py
+++ process_data.py
@@ -4,8 +4,8 @@ import gzip
 import os
 import sys
 from collections import defaultdict
+from datetime import datetime
 
-from decouple import config
 from pyfiglet import Figlet
 
 from aws import AWSSession
@@ -33,15 +33,14 @@ def main(argv):
 
     args = parser.parse_args(argv[1:])
 
-    start_date = args.start_date
-    end_date = args.end_date
+    start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
     output_filename = args.output_filename
 
     aws_session = AWSSession()
-    MAPBOX_KEY = config('MAPBOX_KEY')
+    # MAPBOX_KEY = config('MAPBOX_KEY')
 
     available_dates = aws_session.get_available_dates()
-
     dates_in_range = []
     for date in available_dates:
         if date < start_date:
@@ -64,36 +63,70 @@ def main(argv):
             print('file {0} exists in local storage ... skip'.format(filename))
             available_files.append(file_path)
         else:
-            aws_session.download_object_from_bucket(filename, DATA_PATH)
+            aws_session.download_object_from_bucket(filename, file_path)
 
-    output = defaultdict(lambda: dict(info=dict, dates=defaultdict(lambda: 0)))
+    output = defaultdict(lambda: dict(info=dict(), dates=defaultdict(lambda: 0)))
     for file_path in available_files:
         print('reading file "" ...'.format(os.path.basename(file_path)))
-        with gzip.open(file_path, mode='r', encoding='utf-8') as file_obj:
+        with gzip.open(file_path, str('rt'), encoding='latin-1') as file_obj:
+            file_obj.readline()
             for line in file_obj.readlines():
                 values = line.split(';')
-                stop_code = values[3]
-                stop_name = values[5]
-                area = values[4]
-                date = values[0]
+                stop_code = values[0]
+                stop_name = values[7]
+                area = values[6]
+                date = values[3]
                 transactions = values[8]
 
                 output[stop_code]['info']['name'] = stop_name
                 output[stop_code]['info']['area'] = area
-                output[stop_code]['dates'][date] += transactions
+                output[stop_code]['dates'][date] += int(transactions)
 
     # add location to stop data
-    with open(os.path.join(INPUTS_PATH, 'stop.csv'), encoding='utf-8') as csv_file_obj:
-        spamreader = csv.reader(csv_file_obj, delimiter=';')
+    with open(os.path.join(INPUTS_PATH, 'stop.csv'), encoding='latin-1') as csv_file_obj:
+        spamreader = csv.reader(csv_file_obj, delimiter=',')
         for row in spamreader:
-            stop_code = row[0]
-            stop_longitude = row[1]
-            stop_latitude = row[2]
+            stop_code = row[5]
+            stop_longitude = row[7]
+            stop_latitude = row[8]
 
             output[stop_code]['info']['longitude'] = stop_longitude
             output[stop_code]['info']['latitude'] = stop_latitude
 
-    # TODO: write data to kepler file
+    # save csv data
+    with open(output_filename + '.csv', 'w') as outfile:
+        w = csv.writer(outfile)
+        w.writerow(['fecha', 'nombre', 'area', 'latitud', 'longitud', 'subidas'])
+        for data in dict(output):
+            info = dict(output)[data]['info']
+            longitude = '-'
+            latitude = '-'
+            area = '-'
+            valid = True
+            if 'longitude' in dict(output)[data]['info']:
+                longitude = info['longitude']
+            else:
+                print("Warning: %s doesn't have longitude" % data)
+                valid = False
+
+            if 'latitude' in dict(output)[data]['info']:
+                latitude = info['latitude']
+            else:
+                print("Warning: %s doesn't have latitude" % data)
+                valid = False
+
+            if 'area' in dict(output)[data]['info']:
+                area = info['area']
+            else:
+                print("Warning: %s doesn't have area" % data)
+                valid = False
+
+            name = data
+            for date in dict(output)[data]['dates']:
+                print(date)
+                if valid:
+                    w.writerow([date + " 00:00:00", name, area, longitude, latitude, dict(output)[data]['dates'][date]])
+
 
     # TODO: write mapbox_id to kepler file
 
